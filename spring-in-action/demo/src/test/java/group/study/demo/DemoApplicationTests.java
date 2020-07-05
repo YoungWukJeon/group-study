635af21ec5a2db658154299c051b86bff0ef2910
YoungWukJeon <kdh10080@gmail.com>
2020-07-05 20:40:33 +0900

    initial commit(chapter01)

---
+++ spring-in-action/demo/src/test/java/group/study/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package group.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
