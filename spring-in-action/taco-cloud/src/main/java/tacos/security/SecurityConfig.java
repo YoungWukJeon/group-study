087d34c8b6ff04a3c3e0f9ad9a1eee453f3082bf
YoungWukJeon <kdh10080@gmail.com>
2020-07-25 21:30:52 +0900

    Spring in Action5 책 예제(chapter04) - JDBC 기반의 사용자 스토어

--- spring-in-action/taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ spring-in-action/taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,14 +1,21 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -20,15 +27,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user1")
+//                .password("{noop}password1")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("user2")
+//                .password("{noop}password2")
+//                .authorities("ROLE_USER");
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("{noop}password1")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("user2")
-                .password("{noop}password2")
-                .authorities("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username, password, enabled from users " +
+                                "where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username, authority from authorities " +
+                                "where username=?")
+                .passwordEncoder(new NoEncodingPasswordEncoder());
     }
 }
