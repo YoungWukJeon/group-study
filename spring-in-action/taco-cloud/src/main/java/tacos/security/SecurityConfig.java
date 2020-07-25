86aef10cd559c1f5f1befda494f534d0f98ddc9d
YoungWukJeon <kdh10080@gmail.com>
2020-07-25 23:51:26 +0900

    Spring in Action5 책 예제(chapter04) - 사용자 인증의 커스터마이징

--- spring-in-action/taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ spring-in-action/taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,12 +1,15 @@
 package tacos.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -15,6 +18,13 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -52,19 +62,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .passwordEncoder(new NoEncodingPasswordEncoder());
 //    }
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.ldapAuthentication()
+//            .userSearchBase("ou=people")
+//            .userSearchFilter("(uid={0})")
+//            .groupSearchBase("ou=groups")
+//            .groupSearchFilter("member={0}")
+//            .contextSource()
+//                .root("dc=tacocloud,dc=com")
+//                .ldif("classpath:users.ldif")
+//            .and()
+//                .passwordCompare()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .passwordAttribute("userPasscode");
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.ldapAuthentication()
-            .userSearchBase("ou=people")
-            .userSearchFilter("(uid={0})")
-            .groupSearchBase("ou=groups")
-            .groupSearchFilter("member={0}")
-            .contextSource()
-                .root("dc=tacocloud,dc=com")
-                .ldif("classpath:users.ldif")
-            .and()
-                .passwordCompare()
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .passwordAttribute("userPasscode");
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
     }
 }
