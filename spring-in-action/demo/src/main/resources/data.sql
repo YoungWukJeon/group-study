2c9d117e5d7dac532a235367c0d5c20a50f792be
dydwnsekd <dydwnsekd123@gmail.com>
2020-08-02 16:45:23 +0900

    5장 log 설정

--- spring-in-action/demo/src/main/resources/data.sql
+++ spring-in-action/demo/src/main/resources/data.sql
@@ -6,3 +6,6 @@ values
 ('name', 'category', 'description', 10, '1970-01-01 00:00:00', now()),
 ('name', 'category1', 'description', 10, '1970-01-01 00:00:00', now());
 
+insert into authority(user_no, role, create_date, update_date)
+values
+(1, 'role_user', now(), now());
