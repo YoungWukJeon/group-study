90015f9b4f49dcd6765b71479bd92e0678293611
YoungWukJeon <kdh10080@gmail.com>
2020-08-05 05:04:57 +0900

    header에 버튼 이벤트(메뉴) 추가

---
+++ spring-in-action/demo/src/main/resources/static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#fixed-bar-user-menu").hide();
+
+    $("body").click(function(e) {
+        if (!$("#header-user-profile-button").is(e.target) && 
+            !($("#header-user-profile-button img").is(e.target))) {
+            $("#fixed-bar-user-menu").hide();
+        }
+    });
+
+    $("#header-user-profile-button").click(function() {
+        $("#fixed-bar-user-menu").toggle();
+    });
+});
