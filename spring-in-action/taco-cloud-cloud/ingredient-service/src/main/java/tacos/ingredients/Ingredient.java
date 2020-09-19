5a2145c1b945be96c1e5292c2022470ecf32c044
YoungWukJeon <kdh10080@gmail.com>
2020-09-19 23:48:05 +0900

    Spring in Action5 책 예제(chapter13 - ingredients-service)

---
+++ spring-in-action/taco-cloud-cloud/ingredient-service/src/main/java/tacos/ingredients/Ingredient.java
@@ -0,0 +1,24 @@
+package tacos.ingredients;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PROTECTED, force=true)
+@Entity
+public class Ingredient {
+    @Id
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
---
+++ spring-in-action/taco-cloud-cloud/ingredient-service/src/main/java/tacos/ingredients/IngredientController.java
@@ -0,0 +1,54 @@
+package tacos.ingredients;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path="/ingredients", produces="application/json")
+@CrossOrigin(origins="*")
+public class IngredientController {
+
+    private IngredientRepository repo;
+
+    @Autowired
+    public IngredientController(IngredientRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public Iterable<Ingredient> allIngredients() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Ingredient> byId(@PathVariable String id) {
+        return repo.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateIngredient(@PathVariable String id, @RequestBody Ingredient ingredient) {
+        if (!ingredient.getId().equals(id)) {
+            throw new IllegalStateException("Given ingredient's ID doesn't match the ID in the path.");
+        }
+        repo.save(ingredient);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingredient> postIngredient(@RequestBody Ingredient ingredient) {
+        Ingredient saved = repo.save(ingredient);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("http://localhost:8080/ingredients/" + ingredient.getId()));
+        return new ResponseEntity<>(saved, headers, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteIngredient(@PathVariable String id) {
+        repo.deleteById(id);
+    }
+}
